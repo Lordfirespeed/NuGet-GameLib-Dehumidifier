de69b420a45dc1478c19dfd543c344f1e0b123dd
Joseph Clack <28568841+Lordfirespeed@users.noreply.github.com>
2024-04-09 15:57:24 +0100

    make task bases abstract

--- build/tasks/AsyncFrostingTaskBase.cs
+++ build/tasks/AsyncFrostingTaskBase.cs
@@ -4,7 +4,7 @@ using Cake.Frosting;
 
 namespace Build.Tasks;
 
-public class AsyncFrostingTaskBase<T> : AsyncFrostingTask<T> where T : ICakeContext
+public abstract class AsyncFrostingTaskBase<T> : AsyncFrostingTask<T> where T : ICakeContext
 {
     public override void OnError(Exception exception, T context)
     {
--- build/tasks/FrostingTaskBase.cs
+++ build/tasks/FrostingTaskBase.cs
@@ -4,7 +4,7 @@ using Cake.Frosting;
 
 namespace Build.Tasks;
 
-public class FrostingTaskBase<T> : FrostingTask<T> where T : ICakeContext
+public abstract class FrostingTaskBase<T> : FrostingTask<T> where T : ICakeContext
 {
     public override void OnError(Exception exception, T context)
     {
